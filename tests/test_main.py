7bbf56d03d766dd762a6b9fc9ac5884515c34d01
Shilu <89338633+shilu10@users.noreply.github.com>
2023-03-30 12:55:52 +0530

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -6,7 +6,7 @@ from demo_workflow_pypi import *
 
 def test_add():
     addition = add(1, 3)
-    assert addition == 3, "failed"
+    assert addition == 4, "failed"
 
     sub = sub(2, 1)
     assert sub == 1, "failed"
